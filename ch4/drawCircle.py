2737c84f7ea41b58fe320746d45484105fe99b55
beepscore <support@beepscore.com>
2013-08-29 15:39:07 -0700

    Draw a second circle. Inside while True add a loop.

--- ch4/drawCircle.py
+++ ch4/drawCircle.py
@@ -9,6 +9,8 @@ height = 480
 radius = 100
 stroke = 1
 
+radius2 = 10
+
 # initialize Pygame and all of its submodules
 pygame.init()
 
@@ -18,17 +20,27 @@ window.fill(pygame.Color(0, 0, 0))
 
 # each iteration is like an animation frame
 while True:
+    for i in range(0, 255):
+        xpos = i
+        ypos = i
+        
+        # draw to offscreen buffer
+        pygame.draw.circle(window,
+                           pygame.Color(255, 0, 0),
+                           (width/2, height/2),
+                           radius,
+                           stroke)
+        
+        pygame.draw.circle(window,
+                           pygame.Color(0, 0, 255),
+                           (xpos, ypos),
+                           radius2,
+                           stroke*2)
 
-    # draw to offscreen buffer
-    pygame.draw.circle(window,
-                       pygame.Color(255, 0, 0),
-                       (width/2, height/2),
-                       radius,
-                       stroke)
 
-    # update view
-    pygame.display.update()
-
+        # update view
+        pygame.display.update()
+        
     # enable close button in Pygame window
     if pygame.QUIT in [e.type for e in pygame.event.get()]:
         break
